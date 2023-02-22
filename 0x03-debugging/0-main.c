eecaff546e3718af0e93e0c0a9078c47a3af6e07
Anietie <arrhnnie@gmail.com>
2023-02-22 04:09:40 -0800

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - test numbers and see if it prints positive or negative numbers
+ * Returns: 0
+ */
+
+int main(void)
+{
+	int i:
+	int = 0;
+	positive_or_negative
+	
+	return (0);
+}
