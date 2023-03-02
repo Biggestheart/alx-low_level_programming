c618f0f15f3cf6c46de6f604081836d3db75f01f
Anietie <arrhnnie@gmail.com>
2023-03-02 00:08:38 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,20 +7,32 @@
  *
  *Return: void.
  */
+
 void print_number(int n)
 {
-	unsigned int num;
-/*check if number is negative*/
-	num = n;
+	unsigned int m, d, count;
+
 	if (n < 0)
 	{
 		_putchar(45);
-		num = -n;
+		m = n * -1;
 	}
-/* print number by recursion*/
-	if (num / 10)
+	else
 	{
-		print_number(num / 10);
+		m = n;
 	}
-	_putchar((num % 10) + '0');
+
+	d = m;
+	count = 1;
+
+	while (d > 9)
+	{
+		d /= 10;
+		count *= 10;
+	}
+
+	for (; count >= 1; count /= 10)
+	{
+		_putchar(((m / count) % 10) + 48);
+	}
 }
