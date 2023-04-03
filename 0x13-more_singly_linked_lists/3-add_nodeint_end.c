116cbda5e52ff195dce7cad8e712b0ca4ef93da8
Anietie <arrhnnie@gmail.com>
2023-04-03 16:31:07 -0700

    3-add_nodeint_end.c

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,29 +1,40 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - adds a new node at the end of a listint_t list.
- * @head: pointer to the first element of the list.
- * @n: int to set in the new node.
- * Return: address of the new element, or NULL if it failed
- **/
+ * add_nodeint_end - add a new node at the end
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NUll if it failed.
+ */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *verve, *aux = *head;
+	listint_t *new;
+	listint_t *temp;
 
-	verve = malloc(sizeof(listint_t));
-	if (verve == NULL)
+	(void)temp;
+
+	new = malloc(sizeof(listint_t));
+
+	if (new == NULL)
 		return (NULL);
-	verve->n = n;
-	verve->next = NULL;
 
-	if (aux)
+	new->n = n;
+	new->next = NULL;
+	temp = *head;
+	if (*head == NULL)
 	{
-		while (aux->next)
-			aux = aux->next;
-		aux->next = verve;
+		*head = new;
 	}
 	else
-		*head = verve;
+	{
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
 
-	return (verve);
+	return (*head);
 }
