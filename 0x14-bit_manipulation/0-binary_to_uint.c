6326d8a89ea42eddf34ea1a0594d00904952ad5f
Anietie <arrhnnie@gmail.com>
2023-04-08 17:01:25 -0700

    0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,34 @@
+/*
+ * File_name: 0-binary_to_uint.c
+ * Auth: Essien, Anietie Lawrence
+ * Project: alx-low_level_programming
+ * Status: submitted.
+ */
+
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to an
- * unsigned int.
- * @b: binary.
- *
- * Return: unsigned int.
+ * binary_to_uint - converts a binary number to an unsigned int.
+ * @b: a pointer a string of 0 and 1 chars.
+* Return: If b is NULL or contains chars not 0 or 1 - 0.
+ *         Otherwise - the converted number.
  */
+
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int ui;
-	int len, base_two;
+	int i;
+	unsigned int brny = 0;
 
 	if (!b)
 		return (0);
 
-	ui = 0;
-
-	for (len = 0; b[len] != '\0'; len++)
-		;
-
-	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
+	for (i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
-		{
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		}
-
-		if (b[len] & 1)
-		{
-			ui += base_two;
-		}
+		brny = 2 * brny + (b[i] - '0');
 	}
 
-	return (ui);
+	return (brny);
 }
+
